7c8ba22ccf86b0c470a8ec7ef8f46b400d709d34
mostafanada <mostafanada85110@yahoo.com>
2022-06-02 06:14:17 +0200

    commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { Navigate, Route, Routes } from "react-router-dom";
+import ProductContextProvider from "./contexts/ProductContextProvider";
+import CardContextProvider from "./contexts/CardContextProvider";
+import Store from "./components/Store";
+import DetailsPage from "./components/DetailsPage";
+import Navbar from "./components/common/Navbar";
+import ShopCard from "./components/ShopCard";
+
+const App = () => {
+  return (
+    <ProductContextProvider>
+      <CardContextProvider>
+        <Navbar />
+        <Routes>
+          <Route path="/products" element={<Store />} />
+          <Route path="/cards" element={<ShopCard />} />
+          <Route path="/products/:id" element={<DetailsPage />} />
+          <Route path="/*" element={<Navigate to="/products" />} />
+        </Routes>
+      </CardContextProvider>
+    </ProductContextProvider>
+  );
+};
+
+export default App;
